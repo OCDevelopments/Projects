01aacce9a497c0e7f8a7cd636324ad27bc652e85
Oren <oren002@gmail.com>
2015-11-26 11:18:43 +0200

    fixed the api methods

--- TheMall/TheMallApiService/Controllers/ProductController.cs
+++ TheMall/TheMallApiService/Controllers/ProductController.cs
@@ -8,24 +8,35 @@ using TheMallApiService.Models;
 
 namespace TheMallApiService.Controllers
 {
+    //[Authorize]
     public class ProductController : ApiController
     {
         //Mock
-        Product[] products = new Product[] 
-        { 
+        Product[] products = { 
             new Product { Id = 1, Name = "Chair", Category = "Groceries", Price = 1,Description  = "Include 4 legs!"}, 
             new Product { Id = 2, Name = "Table", Category = "Toys", Price = 3.75M ,Description  = "Include 3 legs!"}, 
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M ,Description  = "Include driller!"} 
         };
 
+        //Get api/Product
+        [HttpGet]
         public IEnumerable<Product> Get()
         {
             return products;
         }
 
-        public IEnumerable<Product> GetById(int id)
+        //Get api/Product/{id}
+        [HttpGet]
+        public IHttpActionResult GetA(int id)
         {
-            return products.Where(o => o.Id == id);
+            var product = products.FirstOrDefault(o => o.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
         }
     }
 }
